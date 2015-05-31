8e474b4bf7eea1f4b7d8f201ea71e848a63b6a5d
Bartosz Marcinkowski <bm371613@gmail.com>
2015-05-31 10:47:08 +0200

    improved fish config

---
+++ _config/fish/config.fish
@@ -0,0 +1,5 @@
+set --erase fish_greeting
+
+set -g fish_color_command 'ffffff'
+set -g fish_color_autosuggestion '666666'
+
--- _config/fish/functions/fish_prompt.fish
+++ _config/fish/functions/fish_prompt.fish
@@ -2,46 +2,36 @@ function fish_prompt --description 'Write out the prompt'
 
     set stat $status
 
-# Just calculate these once, to save a few cycles when displaying the prompt
     if not set -q __fish_prompt_hostname
         set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
     end
 
-if not set -q __fish_prompt_normal
+    if not set -q __fish_prompt_normal
         set -g __fish_prompt_normal (set_color normal)
     end
 
-if not set -q __fish_color_blue
-        set -g __fish_color_blue (set_color -o blue)
-    end
-
-#Set the color for the status depending on the value
     set __fish_color_status (set_color -o green)
+    set __status_string ✔
     if test $stat -gt 0
         set __fish_color_status (set_color -o red)
+        set __status_string ✘
     end
 
-switch $USER
+    switch $USER
 
-case root
+        case root
 
-if not set -q __fish_prompt_cwd
-            if set -q fish_color_cwd_root
-                set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-            else
-                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-            end
-        end
+            set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+            set -g __fish_color_user (set_color -o red)
 
-printf '%s@%s %s%s%s# ' $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
+        case '*'
 
-case '*'
-
-if not set -q __fish_prompt_cwd
             set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-        end
+            set -g __fish_color_user (set_color -o green)
+    end
 
-printf '[%s] %s%s@%s %s%s %s(%s)%s \f\r> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER $__fish_prompt_hostname "$__fish_prompt_cwd" (pwd) "$__fish_color_status" "$stat" "$__fish_prompt_normal"
-
-end
+    printf '%s%s %s%s@%s %s%s %s\f\r➜ ' \
+        "$__fish_color_status" "$__status_string" \
+        "$__fish_color_user" $USER $__fish_prompt_hostname \
+        "$__fish_prompt_cwd" (pwd|sed "s=$HOME=~=") "$__fish_prompt_normal"
 end
