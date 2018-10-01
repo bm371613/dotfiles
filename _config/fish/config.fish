c60a3808ae2b43801596df82a85faf86c8165feb
Bartosz Marcinkowski <bm371613@gmail.com>
2018-10-01 13:34:45 +0200

    fixed fish config

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -1,14 +1,5 @@
 set --erase fish_greeting
 
-for line in (cat ~/.env)
-    export $line
-end
-
-boto
-
-set -gx PATH $HOME/bin $PATH
-set -gx PATH $HOME/.local/bin $PATH
-
 set -g fish_color_autosuggestion '666666'
 set -g fish_color_command 'ffffff'
 set -g fish_color_cwd '5fafd7'
@@ -18,6 +9,17 @@ fish_vi_key_bindings
 switch $TERM
     case xterm
         set TERM xterm-256color
+end
+
+if status --is-login
+    for line in (cat ~/.env)
+        export $line
+    end
+
+    boto
+    set -gx PATH $HOME/bin $PATH
+    set -gx PATH $HOME/.local/bin $PATH
+
 end
 
 keychain -q $HOME/.ssh/id_rsa
